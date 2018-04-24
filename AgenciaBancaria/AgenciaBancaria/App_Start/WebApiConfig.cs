d096d20b9a4f086ec8df3c1269b9159bba2b25aa
misael <misael@DESKTOP-9T0PIVR>
2018-04-24 00:26:06 -0300

    Alteracao substancial

--- AgenciaBancaria/AgenciaBancaria/App_Start/WebApiConfig.cs
+++ AgenciaBancaria/AgenciaBancaria/App_Start/WebApiConfig.cs
@@ -9,11 +9,13 @@ namespace AgenciaBancaria
     {
         public static void Register(HttpConfiguration config)
         {
-            config.MapHttpAttributeRoutes();
+
 
             //Formatar os dados da API em JSON
             config.Formatters.Remove(config.Formatters.XmlFormatter);
             config.Formatters.JsonFormatter.Indent = true;
+
+            config.MapHttpAttributeRoutes();
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
